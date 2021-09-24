8bb37a5978d2bc3d74fa423d905f09577e1b4c5c
mustafakamal <37253065+mustafa-Kmal@users.noreply.github.com>
2021-09-24 19:56:29 +0200

    3-webpack-output-and-loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
-const path = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 module.exports = {
-    entry: './src/client/index.js',
-}
-
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "eslint-loader",
+      },
+    ],
+  },
+};
